7329237c737477b1ea25a3f5399baa21becc6ac1
codingcool21 <bryan.quadras2001@gmail.com>
2015-06-21 15:11:12 +0400

    Removed file

--- shorten.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $longUrl = $_GET["longUrl"];
-    $response = http_post_data("https://www.googleapis.com/urlshortener/v1/url/", array("longUrl"=>$longUrl, "key"=>"AIzaSyB4rbn1o8vo6BYo1ZxyxAXv4-JxCTnjttU"));
-    echo $response;
-?>
